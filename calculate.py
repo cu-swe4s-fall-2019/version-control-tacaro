d9b0e2560bebdcddc4b5682b3c96aa5641d97bc3
tacaro <tristan.caro@colorado.edu>
2019-09-03 12:09:24 -0600

    updated to agree with PEP8 style guide

--- calculate.py
+++ calculate.py
@@ -4,8 +4,8 @@ import math_lib
 a = int(sys.argv[1])
 b = int(sys.argv[2])
 
-c = math_lib.add(a,b)
-d = math_lib.div(a,b)
+c = math_lib.add(a, b)
+d = math_lib.div(a, b)
 
 print("\n\n...running the calculation...\n")
 print("The sum of your inputs is " + str(c))
