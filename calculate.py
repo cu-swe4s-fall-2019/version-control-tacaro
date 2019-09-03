2266fbf06d38a4333c7301be8fa5eeab97a5ddc3
tacaro <tristan.caro@colorado.edu>
2019-09-03 12:02:51 -0600

    calculates both sum and quotient of user supplied arguments

---
+++ calculate.py
@@ -0,0 +1,13 @@
+import sys
+import math_lib
+
+a = int(sys.argv[1])
+b = int(sys.argv[2])
+
+c = math_lib.add(a,b)
+d = math_lib.div(a,b)
+
+print("\n\n...running the calculation...\n")
+print("The sum of your inputs is " + str(c))
+print("The quotient of your inputs is " + str(d))
+print("\n\n")
